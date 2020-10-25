82ae11776da25113caddad33dd66ac9eaa269702
Answerzu <bbeijing92@gmail.com>
2020-10-25 10:42:21 +0300

    feat:add reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let absolute = Math.abs(n);
+    let result = 0;
+    while (absolute) {
+        result = result * 10 + absolute % 10;
+        absolute = Math.floor(absolute / 10);
+    }
+    return (result);
 }
